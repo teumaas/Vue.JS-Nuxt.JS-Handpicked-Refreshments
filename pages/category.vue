12dc453e4955853c5dcd0c2ffd0b63ad4703aad9
Unknown <jlnagelk@student.avans.nl>
2018-06-06 15:39:14 +0200

    category still can't do shit
    * it's not very functional, I'll fix all of it tommorow

--- pages/category.vue
+++ pages/category.vue
@@ -1,46 +1,87 @@
 <template>
   <div>
-    <v-dialog v-model="createDeleteD" persistent max-width="500px">
-      <v-btn slot="activator" color="primary" dark class="mb-2 right">Nieuwe categorie <v-icon right dark>add</v-icon></v-btn>
+    <v-dialog v-model="createD" persistent max-width="500px">
         <v-card>
           <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
+              <span class="headline">Categorie Toevoegen</span>
           </v-card-title>
           <v-card-text>
             <v-container grid-list-md>
               <v-layout wrap>
                 <v-flex xs12>
-                <v-text-field v-model="editedItem.name" label="Category"></v-text-field>
+                  <v-text-field v-model="item.name" label="Naam"></v-text-field>
                 </v-flex>
                 <v-flex xs12>
-                  <v-text-field v-model="editedItem.pictureURL" label="Afbeelding"></v-text-field>          
+                  <v-text-field v-model="item.imageURL" label="Afbeelding"></v-text-field>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click.native="createDeleteD = false">Annuleren</v-btn>
-            <v-btn color="blue darken-1" flat @click="saveC(editedItem)">Opslaan</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="createD = false">Annuleren</v-btn>
+            <v-btn color="blue darken-1" flat @click="createP(item)">Opslaan</v-btn>
           </v-card-actions>
-        </v-card> 
+        </v-card>
     </v-dialog>
 
+    <v-dialog v-model="editD" persistent max-width="500px">
+        <v-card>
+          <v-card-title>
+              <span class="headline">Categorie Bewerken</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12>
+                  <v-text-field v-model="item.name" label="Naam"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field v-model="item.imageURL" label="Afbeelding"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click.native="editD = false">Annuleren</v-btn>
+            <v-btn color="blue darken-1" flat @click="updateP(item)">Opslaan</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-dialog>
 
     <v-dialog v-model="deleteD" persistent max-width="290px">
       <v-card>
-        <v-card-title class="headline">Verwijder categorie</v-card-title>
-        <v-card-text>Weet u zeker dat u deze categorie wil verwijderen?</v-card-text>
+        <v-card-title class="headline">Categorie Verwijderen</v-card-title>
+        <v-card-text>Weet u zeker dat de geselecteerde categorie wilt verwijderen?</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" flat @click.native="deleteD = false">Annuleren</v-btn>
-          <v-btn color="blue darken-1" flat @click="deleteC(deletedItem)">Verwijderen</v-btn>
+          <v-btn color="blue darken-1" flat @click="deleteP(item)">Verwijderen</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="imageD" persistent max-width="290px">
+      <v-card>
+        <v-card-title class="headline">Afbeelding</v-card-title>
+          <v-flex text-xs-center layout align-center justify-center>
+            <v-avatar :tile="false" :size="256">
+              <img :src="item.imageURL" :alt="item.name">
+            </v-avatar>
+          </v-flex>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="imageD = false">Sluiten</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
     <v-card-title>
-      <v-spacer></v-spacer> 
+      <v-flex style="margin-top: 15px;" xs12 sm6 text-xs-left>
+        <v-btn color="primary" @click="createItem()">Nieuwe categorie <v-icon dark> add</v-icon></v-btn>
+      </v-flex>
+      <v-spacer></v-spacer>
       <v-text-field v-model="search" append-icon="search" label="Zoeken..." single-line hide-details></v-text-field>
     </v-card-title>   
     <v-data-table :headers="headers" :items="categories" :search="search" hide-actions class="elevation-1">
@@ -48,7 +89,11 @@
         <td class="text-xs-left"></td>
         <td class="text-xs-left">{{ props.item.categoryID }}</td>
         <td class="text-xs-left">{{ props.item.name }}</td>
-        <td class="text-xs-left">{{ props.item.pictureURL }}</td>
+        <td class="text-xs-left">
+          <v-avatar class="justify-left">
+            <img :src="props.item.imageURL" @click="imageItem(props.item)">    
+          </v-avatar>
+        </td>
         <td class="justify-left layout px-0">
           <v-btn icon class="mx-0" @click="editItem(props.item)">
             <v-icon color="teal">edit</v-icon>
@@ -74,47 +119,55 @@
   /* DataTable settings */
   export default {
     data: () => ({
-      createDeleteD: false,
+      createD: false,
+      editD: false,
       deleteD: false,
+      imageD: false,
       search: '',
+      hidden: false,
       headers: [
         {
           align: 'left',
           sortable: false,
           value: 'name'
         },
-        { text: 'Categorie ID', value: 'categoryID' },
+        { text: 'Category ID', value: 'categoryID' },
         { text: 'Naam', value: 'name' },
-        { text: 'Afbeelding', value: 'pictureURL' },
+        { text: 'Afbeelding', value: 'imageURL' },
         { text: 'Acties', value: 'name', sortable: false }
       ],
       categories: [],
-      editedIndex: -1,
-      editedItem: {
+      itemIndex: -1,
+      item: {
         categoryID: 0,
         name: '',
-        pictureURL: ''
+        imageURL: ''
       },
       defaultItem: {
         categoryID: 0,
         name: '',
-        pictureURL: ''
+        imageURL: ''
       }
     }),
 
     computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'Nieuwe Categorie' : 'Bewerk Categorieën'
-      }
     },
 
     /* Add here new function for new dialog */
     watch: {
-      createDeleteD (val) {
+      createD (val) {
+        val || this.close()
+      },
+
+      editD (val) {
         val || this.close()
       },
 
       deleteD (val) {
+        val || this.close()
+      },
+
+      imageD (val) {
         val || this.close()
       }
     },
@@ -126,34 +179,48 @@
 
     methods: {
       /* UI Logic for CRUD Functions. */
+      createItem () {
+        this.createD = true
+      },
+
       editItem (item) {
-        this.editedIndex = this.categories.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.createDeleteD = true
+        this.itemIndex = this.categories.indexOf(item)
+        this.item = Object.assign({}, item)
+        this.editD = true
       },
 
       deleteItem (item) {
-        this.deletedIndex = this.categories.indexOf(item)
-        this.deletedItem = Object.assign({}, item)
+        this.itemIndex = this.categories.indexOf(item)
+        this.item = Object.assign({}, item)
         this.deleteD = true
       },
 
+      imageItem (item) {
+        this.itemIndex = this.categories.indexOf(item)
+        this.item = Object.assign({}, item)
+        this.imageD = true
+      },
+
       close () {
-        this.createDeleteD = false
+        this.createD = false
+        this.imageD = false
+        this.editD = false
         this.deleteD = false
         setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.deleteItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-          this.deletedIndex = -1
+          this.item = Object.assign({}, this.defaultItem)
+          this.itemIndex = -1
         }, 300)
       },
 
-      saveC (item) {
-        this.editCategory(item)
+      createP (item) {
+        this.postCategories(item)
       },
 
-      deleteC (item) {
+      updateP (item) {
+        this.updateCategory(item)
+      },
+
+      deleteP (item) {
         this.deleteCategory(item)
       },
 
@@ -164,12 +231,12 @@
         })
       },
 
-      postCategory (item) {
+      postCategories (item) {
         axios.post(`https://handpicked-refreshments.herokuapp.com/api/category/`, {
           name: item.name,
-          imageURL: item.pictureURL
+          imageURL: item.imageURL
         },
-        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+        { headers: { 'Content-Type': 'application/json; charset=utf-8' } }
         ).then(function (response) {
           console.log(response)
         }).catch(function (error) {
@@ -178,12 +245,12 @@
         this.close()
       },
 
-      editCategory (item) {
-        axios.put(`https://handpicked-refreshments.herokuapp.com/api/category` + item.categoryID, {
+      updateCategory (item) {
+        axios.put(`https://handpicked-refreshments.herokuapp.com/api/category/` + item.categoryID, {
           name: item.name,
-          pictureURL: item.pictureURL
+          imageURL: item.imageURL
         },
-        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+        { headers: { 'Content-Type': 'application/json; charset=utf-8' } }
         ).then(function (response) {
           console.log(response)
         }).catch(function (error) {
