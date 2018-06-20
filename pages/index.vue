8d987f9072f599a18d39de5ed299b29fb82ab273
Tom Smits <tmsmits@gmail.com>
2018-06-20 12:56:33 +0200

    Improvements index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@
         <img src="icon.png" size=32px alt="Vuetify.js" class="mb-5" />
       </div>
       <v-card>
-        <v-card-title class="headline">Handpicked Refreshments</v-card-title>
         <v-card-text>
           <p>Welkom bij het Handpicked Refreshments administrator controlpanel.</p>
         </v-card-text>
